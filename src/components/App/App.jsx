9e8751d76f42baf2e0f64f2399d97bd2984df6d2
Jaime Sanders <jaimelyn517@gmail.com>
2020-08-20 12:03:04 -0700

    creates JoinRoom component and renders on page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import SignUpForm from '../SignUpForm/SignUpForm';
 import LogInForm from '../LogInForm/LogInForm';
+import CreateRoom from '../CreateRoom/CreateRoom';
+import JoinRoom from '../JoinRoom/JoinRoom';
 
 export default function App() {
   return <>
     <SignUpForm/>
-    <LogInForm />
+    <LogInForm/>
+    <CreateRoom/>
+    <JoinRoom/>
   </>;
 }
