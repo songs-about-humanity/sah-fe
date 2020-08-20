85bf9cd6797bc8b9604c5e5d6e2b13af50288baf
Jaime Sanders <jaimelyn517@gmail.com>
2020-08-20 13:15:32 -0700

    makes a room component and sets up router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import SignUpForm from '../SignUpForm/SignUpForm';
 import LogInForm from '../LogInForm/LogInForm';
 import CreateRoom from '../CreateRoom/CreateRoom';
 import JoinRoom from '../JoinRoom/JoinRoom';
 
 export default function App() {
-  return <>
-    <SignUpForm/>
-    <LogInForm/>
-    <CreateRoom/>
-    <JoinRoom/>
-  </>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/signup" component={SignUpForm}/>
+        <Route path="/login" component={LogInForm} />
+        <Route path="/create" component={CreateRoom} />
+        <Route path="/join" component={JoinRoom} />
+      </Switch>
+    </Router>
+  );
 }
---
+++ src/components/Room/Room.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Room() {
+  return (
+    <div>
+      <p>You entered a room!</p>
+    </div>
+  );
+}
+
