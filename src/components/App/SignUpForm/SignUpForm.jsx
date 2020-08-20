bca42a2810f392d08f4d0b9cd965396d5f89228c
Jaime Sanders <jaimelyn517@gmail.com>
2020-08-20 11:29:30 -0700

    creates sign up form

---
+++ src/components/App/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+export default function SignUpForm() {
+  const [username, setUserName] = useState('');
+  const [pin, setPIN] = useState('');
+
+  const handleChange = ({ target }) => {
+    if(target.name === 'username') setUserName(target.value);
+    if(target.name === 'pin') setPIN(target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(username, pin);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="username"
+        placeholder="User Name"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="pin"
+        placeholder="PIN"
+        onChange={handleChange}
+      />
+      <button>Sign Up</button>
+    </form>
+  );
+}
+
