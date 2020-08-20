b70070d210acac09f8741f9bafb94c985b385055
Jaime Sanders <jaimelyn517@gmail.com>
2020-08-20 11:34:10 -0700

    renders sign up form in browser

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import SignUpForm from './SignUpForm/SignUpForm';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <>
+    <SignUpForm/>
+  </>;
 }
-  
